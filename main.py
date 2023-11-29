d73a5c35d0324fdf35b65e0de833e9dfd2c51796
Florian Dietsch <florian.dietsch@dlh.de>
2023-11-29 10:03:50 +0100

    move wichtel.txt to be part of the tool itself. easier to create .exe without any files that need to be present while running it

--- main.py
+++ main.py
@@ -1,33 +1,35 @@
 import random
 
-
-def wichteln_teilnehmer_aus_datei(dateiname):
-    try:
-        with open(dateiname, 'r') as file:
-            teilnehmer = file.read().split(',')
-            teilnehmer = [name.strip() for name in teilnehmer]
-        return teilnehmer
-    except FileNotFoundError:
-        print(f"Die Datei {dateiname} wurde nicht gefunden.")
-        return []
-
+def wichteln_teilnehmer_aus_liste():
+    teilnehmer = [
+        "Florian",
+        "Markus",
+        "Claudia",
+        "Jenny",
+        "Jue",
+        "Christian",
+        "Rosi",
+        "Heike",
+        "Helmut",
+        "Günther",
+        # Füge hier weitere Teilnehmer hinzu
+    ]
+    return teilnehmer
 
 def wichteln_paarungen_erstellen(teilnehmer):
     paare = list(zip(teilnehmer, teilnehmer[1:] + [teilnehmer[0]]))
     random.shuffle(paare)
     return paare
 
-
 def wichteln_paarungen_in_textdatei_speichern(paare, dateiname='wichtel_paarungen.txt'):
     with open(dateiname, 'w') as file:
         for schenker, beschenkter in paare:
             file.write(f"{schenker} beschenkt {beschenkter}\n")
 
-
 if __name__ == "__main__":
     print("Willkommen beim Wichteln Programm!")
 
-    teilnehmer = wichteln_teilnehmer_aus_datei("wichtel.txt")
+    teilnehmer = wichteln_teilnehmer_aus_liste()
 
     if len(teilnehmer) < 2:
         print("Es müssen mindestens zwei Teilnehmer vorhanden sein.")
